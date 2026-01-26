ec568ef15ce074b76fe783e8d4663ec1293f0097
Sameer Meher <sam.tiku@gmail.com>
2026-01-26 17:21:46 +0530

    Configure DevOps project

--- src/app.py
+++ src/app.py
@@ -2,18 +2,27 @@ from flask import Flask, jsonify
 import datetime
 import socket
 
+
 app = Flask(__name__)
 
-@app.route('/api/v1/details')
-def details():
+
+@app.route('/api/v1/info')
+
+def info():
     return jsonify({
-        "time": datetime.datetime.now().strftime("%I:%M:%S %p on %B %d, %Y"),
-        "hostname": socket.gethostname()
+    	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
+    	'hostname': socket.gethostname(),
+        'message': 'You are doing great, little human! <3',
+        'deployed_on': 'kubernetes'
     })
 
 @app.route('/api/v1/healthz')
+
 def health():
+	# Do an actual check here
     return jsonify({'status': 'up'}), 200
 
 if __name__ == '__main__':
-    app.run()
+
+    app.run(host="0.0.0.0")
+
