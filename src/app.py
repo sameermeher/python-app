e9fb66b7dfd6a79f53aa7733823d4e107c173699
Sameer Meher <sam.tiku@gmail.com>
2026-01-26 17:31:35 +0530

    revert back changes

--- src/app.py
+++ src/app.py
@@ -5,24 +5,17 @@ import socket
 
 app = Flask(__name__)
 
-
-@app.route('/api/v1/info')
-
-def info():
+@app.route('/api/v1/details')
+def details():
     return jsonify({
-    	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
-    	'hostname': socket.gethostname(),
-        'message': 'You are doing great, little human! <3',
-        'deployed_on': 'kubernetes'
+        "time": datetime.datetime.now().strftime("%I:%M:%S %p on %B %d, %Y"),
+        "hostname": socket.gethostname()
     })
 
 @app.route('/api/v1/healthz')
-
 def health():
-	# Do an actual check here
     return jsonify({'status': 'up'}), 200
 
 if __name__ == '__main__':
-
-    app.run(host="0.0.0.0")
+    app.run()
 
